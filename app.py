e69d43e241ffb48aebd25e5f9b6a98eace59cc4b
unknown <Karthikeya>
2021-01-18 19:51:31 +0530

    coment removed

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def homePage():
     return render_template("index.html")
 
-@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
+@app.route('/review',methods=['POST']) # route to show the review comments in a web UI
 @cross_origin()
 def index():
     if request.method == 'POST':
@@ -74,7 +74,7 @@ def index():
         except Exception as e:
             print('The Exception message is: ',e)
             return 'something is wrong'
-    # return render_template('results.html')
+        return render_template('results.html')
 
     else:
         return render_template('index.html')
